a8eef25f3c21cbb58d933f0fb408a5c01bdbdb96
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2024-11-05 12:49:59 +0600

    Hero Responsive

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,8 +1,10 @@
-import logo from '../../../assets/logo.png';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { IoMenuSharp } from "react-icons/io5";
+import { IoMenuSharp, IoCloseSharp } from "react-icons/io5";
+import logo from '../../../assets/logo.png';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <div className="w-[full] md:h-[108px] h-[77px] flex justify-between items-center md:px-32 px-8">
             <div className="flex items-center gap-[60px]">
@@ -17,7 +19,18 @@ const Header = () => {
                 </ul>
             </div>
             <button className="common-btn hidden md:block">Book A Table</button>
-            <IoMenuSharp className="text-white text-4xl md:hidden block" />
+            <IoMenuSharp className="text-white text-4xl cursor-pointer transition-transform duration-500 ease-in-out md:hidden block" onClick={() => setIsMenuOpen(true)} />
+            {isMenuOpen &&
+                <div className="md:hidden block absolute top-0 left-0 w-full h-fit bg-background-primary z-50 transition-all duration-500 ease-in-out">
+                    <div className='w-full h-fit bgSVG py-24'>
+                        <IoCloseSharp className="text-white text-4xl cursor-pointer absolute top-6 right-8 transition-transform duration-500 ease-in-out" onClick={() => setIsMenuOpen(false)} />
+                        <ul className="flex flex-col items-center justify-center h-full gap-10 text-white font-raleway text-[15px] font-[500]">
+                            {navItems.map((item, index) => (
+                                <li key={index} className='hover:text-yellow transition-all duration-300 ease-in-out hover:scale-105 active:scale-95'><Link to={item.path}>{item.name}</Link></li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>}
         </div>
     );
 };
